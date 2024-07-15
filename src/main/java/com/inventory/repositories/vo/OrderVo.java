e92d29f1778657b5623f0f0b67c2d6c94bfe360f
wwjk4628 <163090464+wwjk4628@users.noreply.github.com>
2024-07-15 17:48:18 +0900

    발주 승인 페이지

--- src/main/java/com/inventory/repositories/vo/OrderVo.java
+++ src/main/java/com/inventory/repositories/vo/OrderVo.java
@@ -5,14 +5,17 @@ import java.util.Date;
 
 public class OrderVo {
 
-    private String orderId;
-    private String branchId;
-    private Date orderDate;
-    private String checked;
-    private String bookCode;
-    private String bookName;
-    private String branchName;
-    public String getBranchName() {
+	private String orderId;
+	private String branchId;
+	private Date orderDate;
+	private String checked;
+	private String bookCode;
+	private String bookName;
+	private String branchName;
+	private int price;
+	private int quantity;
+
+	public String getBranchName() {
 		return branchName;
 	}
 
@@ -20,11 +23,9 @@ public class OrderVo {
 		this.branchName = branchName;
 	}
 
-	private int price;
-    private int quantity;
-    private String userName;
+	private String userName;
 
-    public String getUserName() {
+	public String getUserName() {
 		return userName;
 	}
 
@@ -33,122 +34,122 @@ public class OrderVo {
 	}
 
 	// Default constructor
-    public OrderVo() {
-    }
+	public OrderVo() {
+	}
 
-    // Constructor with branchId
-    public OrderVo(String branchId) {
-        this.branchId = branchId;
-    }
+	// Constructor with branchId
+	public OrderVo(String branchId) {
+		this.branchId = branchId;
+	}
 
-    // Constructor with bookCode, bookName, and quantity
-    public OrderVo(String bookCode, String bookName, int quantity) {
-        this.bookCode = bookCode;
-        this.bookName = bookName;
-        this.quantity = quantity;
-    }
+	// Constructor with bookCode, bookName, and quantity
+	public OrderVo(String bookCode, String bookName, int quantity) {
+		this.bookCode = bookCode;
+		this.bookName = bookName;
+		this.quantity = quantity;
+	}
 
+	// Constructor for order list verification
+	public OrderVo(String orderId, String branchId, Date orderDate, String checked) {
+		this.orderId = orderId;
+		this.branchId = branchId;
+		this.orderDate = orderDate;
+		this.checked = checked;
+	}
 
-    // Constructor for order list verification
-    public OrderVo(String orderId, String branchId, Date orderDate, String checked) {
-        this.orderId = orderId;
-        this.branchId = branchId;
-        this.orderDate = orderDate;
-        this.checked = checked;
-    }
+	// Full constructor
+	public OrderVo(String orderId, String branchId, Date orderDate, String checked, String bookCode, int quantity) {
+		this.orderId = orderId;
+		this.branchId = branchId;
+		this.orderDate = orderDate;
+		this.checked = checked;
+		this.bookCode = bookCode;
+		this.quantity = quantity;
+	}
 
-    // Full constructor
-    public OrderVo(String orderId, String branchId, Date orderDate, String checked, String bookCode, int quantity) {
-        this.orderId = orderId;
-        this.branchId = branchId;
-        this.orderDate = orderDate;
-        this.checked = checked;
-        this.bookCode = bookCode;
-        this.quantity = quantity;
-    }
+	// Full constructor with bookName and price
+	public OrderVo(String orderId, String branchId, Date orderDate, String checked, String bookCode, String bookName,
+			int price, int quantity) {
+		this.orderId = orderId;
+		this.branchId = branchId;
+		this.orderDate = orderDate;
+		this.checked = checked;
+		this.bookCode = bookCode;
+		this.bookName = bookName;
+		this.price = price;
+		this.quantity = quantity;
+	}
 
-    // Full constructor with bookName and price
-    public OrderVo(String orderId, String branchId, Date orderDate, String checked, String bookCode, String bookName, int price, int quantity) {
-        this.orderId = orderId;
-        this.branchId = branchId;
-        this.orderDate = orderDate;
-        this.checked = checked;
-        this.bookCode = bookCode;
-        this.bookName = bookName;
-        this.price = price;
-        this.quantity = quantity;
-    }
+	// Getters and Setters
+	public String getOrderId() {
+		return orderId;
+	}
 
-    // Getters and Setters
-    public String getOrderId() {
-        return orderId;
-    }
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
 
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
+	public String getBranchId() {
+		return branchId;
+	}
 
-    public String getBranchId() {
-        return branchId;
-    }
+	public void setBranchId(String branchId) {
+		this.branchId = branchId;
+	}
 
-    public void setBranchId(String branchId) {
-        this.branchId = branchId;
-    }
+	public String getOrderDate() {
+		if (orderDate == null) {
+			return "";
+		}
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yy/MM/dd HH:mm");
+		return dateFormat.format(orderDate);
+	}
 
-    public String getOrderDate() {
-        if (orderDate == null) {
-            return "";
-        }
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yy/MM/dd HH:mm");
-        return dateFormat.format(orderDate);
-    }
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
 
-    public void setOrderDate(Date orderDate) {
-        this.orderDate = orderDate;
-    }
+	public String getChecked() {
+		return checked;
+	}
 
-    public String getChecked() {
-        return checked;
-    }
+	public void setChecked(String checked) {
+		this.checked = checked;
+	}
 
-    public void setChecked(String checked) {
-        this.checked = checked;
-    }
+	public String getBookCode() {
+		return bookCode;
+	}
 
-    public String getBookCode() {
-        return bookCode;
-    }
+	public void setBookCode(String bookCode) {
+		this.bookCode = bookCode;
+	}
 
-    public void setBookCode(String bookCode) {
-        this.bookCode = bookCode;
-    }
+	public int getQuantity() {
+		return quantity;
+	}
 
-    public int getQuantity() {
-        return quantity;
-    }
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
+	public String getBookName() {
+		return bookName;
+	}
 
-    public String getBookName() {
-        return bookName;
-    }
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
 
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
+	public int getPrice() {
+		return price;
+	}
 
-    public int getPrice() {
-        return price;
-    }
+	public void setPrice(int price) {
+		this.price = price;
+	}
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    @Override
+	@Override
 	public String toString() {
 		return "OrderVo [orderId=" + orderId + ", branchId=" + branchId + ", orderDate=" + orderDate + ", checked="
 				+ checked + ", bookCode=" + bookCode + ", bookName=" + bookName + ", branchName=" + branchName
--- src/main/webapp/WEB-INF/views/admins/order_check_list.jsp
+++ src/main/webapp/WEB-INF/views/admins/order_check_list.jsp
@@ -33,9 +33,9 @@
                     <td>${vo.userName }</td>
                     <td>
                         <c:choose>
-                            <c:when test="${vo.checked eq 0}">미확인</c:when>
-                            <c:when test="${vo.checked eq 1}">반려</c:when>
-                            <c:when test="${vo.checked eq 2}">처리 완료</c:when>
+                            <c:when test="${vo.checked eq 0}" ><span style="color: red;">미확인</span></c:when>
+                            <c:when test="${vo.checked eq 1}" ><span style="color: yellow;">반려</span></c:when>
+                            <c:when test="${vo.checked eq 2}" ><span style="color: green;">처리 완료</span></c:when>
                             <c:otherwise>알 수 없음</c:otherwise>
                         </c:choose>
                     </td>
